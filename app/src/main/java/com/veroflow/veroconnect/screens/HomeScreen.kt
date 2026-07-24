a0120d41482661717b300cec7f4be129a4d26eb4
UsmanProjects <usmankhan122005@gmail.com>
2026-07-24 13:13:53 +0500

    Update HomeScreen.kt

--- app/src/main/java/com/veroflow/veroconnect/screens/HomeScreen.kt
+++ app/src/main/java/com/veroflow/veroconnect/screens/HomeScreen.kt
@@ -1,13 +1,22 @@
 package com.veroflow.veroconnect.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.veroflow.veroconnect.data.MockData
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
     Scaffold(
@@ -25,7 +34,6 @@ fun HomeScreen(navController: NavController) {
                         Text(post.username)
                         Text(post.content)
                         Text("Likes: ${post.likes}")
-                        // Buttons for like, comment etc.
                     }
                 }
             }
